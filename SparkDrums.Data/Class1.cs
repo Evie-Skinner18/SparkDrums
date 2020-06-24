b616a6469542dcea2152645bfe7938622d35fe69
Evie Skinner <evie.skinner@teamriver.com>
2020-06-24 12:54:36 +0100

    Created all projects and sln file for backend. Gone for NUnit test project for now as I wasnt keen on XUnit

---
+++ SparkDrums.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SparkDrums.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ SparkDrums.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SparkDrums.Services
+{
+    public class Class1
+    {
+    }
+}
