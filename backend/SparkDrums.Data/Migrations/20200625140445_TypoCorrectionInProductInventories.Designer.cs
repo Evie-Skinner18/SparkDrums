28e7f39ca7770517af13200cade8e86e54ba0cf4
Evie Skinner <evie.skinner@teamriver.com>
2020-06-25 15:09:22 +0100

    Added new migration to correct table name typo

--- backend/SparkDrums.Data/Migrations/20200625140445_TypoCorrectionInProductInventories.Designer.cs
+++ backend/SparkDrums.Data/Migrations/20200625140445_TypoCorrectionInProductInventories.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SparkDrums.Data;
@@ -9,9 +10,10 @@ using SparkDrums.Data;
 namespace SparkDrums.Data.Migrations
 {
     [DbContext(typeof(SparkDrumsDbContext))]
-    partial class SparkDrumsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200625140445_TypoCorrectionInProductInventories")]
+    partial class TypoCorrectionInProductInventories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/SparkDrums.Data/Migrations/20200625140445_TypoCorrectionInProductInventories.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SparkDrums.Data.Migrations
+{
+    public partial class TypoCorrectionInProductInventories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductInventoriess");
+
+            migrationBuilder.CreateTable(
+                name: "ProductInventories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductId = table.Column<int>(nullable: true),
+                    QuantityAvailable = table.Column<int>(nullable: false),
+                    IdealQuantity = table.Column<int>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    UpdatedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInventories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductInventories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInventories_ProductId",
+                table: "ProductInventories",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductInventories");
+
+            migrationBuilder.CreateTable(
+                name: "ProductInventoriess",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    IdealQuantity = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: true),
+                    QuantityAvailable = table.Column<int>(type: "integer", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInventoriess", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductInventoriess_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInventoriess_ProductId",
+                table: "ProductInventoriess",
+                column: "ProductId");
+        }
+    }
+}
